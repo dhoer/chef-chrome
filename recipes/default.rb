474e21b9046e370ea41deb17002f831e950ca7d1
Dennis Hoer <dennis.hoer@pearson.com>
2014-10-28 09:26:39 -0700

    Warn if not windows platform

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,8 @@
-windows_package 'Google Chrome' do
-  source node['chrome']['src']
-  action :install
+if platform?('windows')
+  windows_package 'Google Chrome' do
+    source node['chrome']['src']
+    action :install
+  end
+else
+  Chef::Log.warn('Chrome can only be installed on the Windows platform using this cookbook.')
 end
--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe 'chrome::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::Runner.new(platform: 'windows', version: '2008R2').converge(described_recipe)
+  end
 
   it 'installs google chrome' do
     expect(chef_run).to install_windows_package('Google Chrome')
