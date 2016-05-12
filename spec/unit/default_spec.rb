2234b4ab38890aa0d6e01db62d90a336b4ae6659
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-12 13:07:20 -0700

    Fix failing tests

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe 'chrome_test::default' do
   context 'msi' do
-    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'windows', version: '2008R2').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'windows', version: '2008R2') do
+      allow_any_instance_of(Chef::Recipe).to receive(:chrome_version).and_return('38.0.2125.234')
+    end.converge(described_recipe) }
 
     it 'installs google' do
       expect(chef_run).to install_windows_package('Google Chrome')
@@ -11,6 +13,9 @@ describe 'chrome_test::default' do
 
   context 'dmg' do
     let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.7.4').converge(described_recipe) }
+    let(:shellout) { double(run_command: nil, error!: nil, stdout: 'Google Chrome 38.0.2125.234 ') }
+
+    before { allow(Mixlib::ShellOut).to receive(:new).and_return(shellout) }
 
     it 'installs chrome' do
       expect(chef_run).to install_dmg_package('Google Chrome')
@@ -19,6 +24,9 @@ describe 'chrome_test::default' do
 
   context 'yum' do
     let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0').converge(described_recipe) }
+    let(:shellout) { double(run_command: nil, error!: nil, stdout: 'Google Chrome 38.0.2125.234 ') }
+
+    before { allow(Mixlib::ShellOut).to receive(:new).and_return(shellout) }
 
     it 'adds repo' do
       expect(chef_run).to add_yum_repository('google-chrome')
@@ -31,10 +39,9 @@ describe 'chrome_test::default' do
 
   context 'apt' do
     let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
+    let(:shellout) { double(run_command: nil, error!: nil, stdout: 'Google Chrome 38.0.2125.234 ') }
 
-    it 'performs apt-get update' do
-      expect(chef_run).to run_execute('apt-get update')
-    end
+    before { allow(Mixlib::ShellOut).to receive(:new).and_return(shellout) }
 
     it 'adds repo' do
       expect(chef_run).to add_apt_repository('google-chrome')
--- test/fixtures/cookbooks/chrome_test/recipes/default.rb
+++ test/fixtures/cookbooks/chrome_test/recipes/default.rb
@@ -1,4 +1,3 @@
-include_recipe 'apt'
 include_recipe 'chrome'
 include_recipe 'chrome_test::version'
 include_recipe 'chrome_test::master_preferences'
