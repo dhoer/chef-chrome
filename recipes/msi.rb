8354c198c6cade2d836cdf4754cc3ad6fa65c574
Ravindrakumar Solanki <ravindrakumars@interxion.com>
2017-05-11 14:29:27 +0100

    replaced empty with nil as it was causing an error

--- recipes/msi.rb
+++ recipes/msi.rb
@@ -2,5 +2,5 @@ chrome64 = node['kernel']['machine'] == 'x86_64' && !node['chrome']['32bit_only'
 windows_package 'Google Chrome' do
   source chrome64 ? node['chrome']['msi_64'] : node['chrome']['msi']
   action :nothing
-  only_if { (chrome_windows_version).empty? }
+  only_if { chrome_windows_version.nil? }
 end.run_action(:install)
