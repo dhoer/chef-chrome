b981c4ed316ebc541b1bb7a3189c6f9dd4a01a99
Dennis Hoer <dennis.hoer@pearson.com>
2017-05-19 11:26:23 -0700

    Fix msi install test on appveyor

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -4,7 +4,7 @@ describe 'chrome::default' do
   context 'msi' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'windows', version: '2008R2') do
-        allow_any_instance_of(Chef::Recipe).to receive(:chrome_version).and_return('38.0.2125.234')
+        # allow_any_instance_of(Chef::Recipe).to receive(:chrome_version).and_return('38.0.2125.234')
         allow_any_instance_of(::File).to receive(:directory?).and_return(false)
       end.converge(described_recipe)
     end
