bb39cba76869245d6afd6c2cc60f06dacbb87a12
Dennis Hoer <dennis.hoer@pearson.com>
2017-05-19 12:40:01 -0700

    Fix msi install test on appveyor

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe 'chrome::default' do
-  context 'msi' do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'windows', version: '2008R2') do
-        allow_any_instance_of(Chef::Recipe).to receive(:chrome_version).with(:nil?).and_return(true)
-      end.converge(described_recipe)
-    end
+  if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
+    context 'msi' do
+      let(:chef_run) do
+        ChefSpec::SoloRunner.new(platform: 'windows', version: '2008R2').converge(described_recipe)
+      end
 
-    it 'installs google' do
-      expect(chef_run).to install_windows_package('Google Chrome')
+      it 'installs google' do
+        expect(chef_run).to install_windows_package('Google Chrome')
+      end
     end
-  end if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
+  end
 
   context 'dmg' do
     let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.12').converge(described_recipe) }
